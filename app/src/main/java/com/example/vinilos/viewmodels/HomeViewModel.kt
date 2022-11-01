77f494028e39c76be5e6031c55c1592d81f3f311
VivianaReyV <z.rey@uniandes.edu.co>
2022-11-01 17:08:39 -0500

    home view creation

--- app/src/main/java/com/example/vinilos/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/vinilos/viewmodels/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Bienvenido!"
     }
     val text: LiveData<String> = _text
 }
