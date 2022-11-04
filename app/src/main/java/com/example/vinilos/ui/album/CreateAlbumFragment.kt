0ad9102b810c46d7123314dbb53c07aa65626bec
ErikBernal94 <ed.bernalc1@uniandes.edu.co>
2022-11-03 23:25:42 -0500

    Validación de campos en la creación de albúm

--- app/src/main/java/com/example/vinilos/ui/album/CreateAlbumFragment.kt
+++ app/src/main/java/com/example/vinilos/ui/album/CreateAlbumFragment.kt
@@ -58,6 +58,45 @@ class CreateAlbumFragment: Fragment() {
     }
 
     fun createAlbum(view: View){
+
+        if (binding.nombreAlbum.text.toString() == null || binding.nombreAlbum.text.toString() == "")
+        {
+            formError("El campo nombre no puede estar vacío")
+            return
+        }
+        if (binding.cover.text.toString() == null || binding.cover.text.toString() == "")
+        {
+            formError("El campo Portada no puede estar vacío")
+            return
+        }
+        if (binding.descripcion.text.toString() == null || binding.descripcion.text.toString() == "")
+        {
+            formError("El campo Descripción no puede estar vacío")
+            return
+        }
+        if (binding.genero.text.toString() != " Clasical" &&
+            binding.genero.text.toString() != "Salsa" &&
+            binding.genero.text.toString() != "Rock" &&
+            binding.genero.text.toString() != "Folk")
+        {
+            formError("El campo Género debe ser: Classical, Salsa, Rock o Folk")
+            return
+        }
+        if (binding.discografia.text.toString() != " Sony Music" &&
+            binding.discografia.text.toString() != "EMI" &&
+            binding.discografia.text.toString() != "Discos Fuentes" &&
+            binding.discografia.text.toString() != "Elektra" &&
+            binding.discografia.text.toString() != "Fania Records")
+        {
+            formError("El campo Discografia debe ser: Sony Music, EMI, Discos Fuentes, Elektra o Fania Records")
+            return
+        }
+        val regex = Regex("^\\d{4}-\\d{2}-\\d{2}$")
+        if (!regex.matches(binding.fechaLanzamiento.text.toString()))
+        {
+            formError("El campo Fecha de lanzamiento no es válido")
+            return
+        }
         val albumToCreate = Album(null,
             binding.nombreAlbum.text.toString(),
             binding.cover.text.toString(),
@@ -83,5 +122,9 @@ class CreateAlbumFragment: Fragment() {
             Toast.makeText(activity, "Error al crear albúm", Toast.LENGTH_LONG).show()
             viewModel.onNetworkErrorShown()
         }
+    }
+
+    private fun formError(error: String){
+        Toast.makeText(activity, error, Toast.LENGTH_LONG).show()
     }
 }
