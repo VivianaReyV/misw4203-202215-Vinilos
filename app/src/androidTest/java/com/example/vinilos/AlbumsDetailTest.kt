81b4687d45b9a7b193ae54e958b0494aeb4490a9
Jeyson Vega <jvega@univision.net>
2022-11-27 15:55:23 -0500

    fix talkback readibility

--- app/src/androidTest/java/com/example/vinilos/AlbumsDetailTest.kt
+++ app/src/androidTest/java/com/example/vinilos/AlbumsDetailTest.kt
@@ -1,18 +1,14 @@
 package com.example.vinilos
 
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.EnumSet.allOf
 
 
 /**
--- app/src/androidTest/java/com/example/vinilos/AlbumsListTest.kt
+++ app/src/androidTest/java/com/example/vinilos/AlbumsListTest.kt
@@ -6,7 +6,6 @@ import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
